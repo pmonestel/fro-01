be482fff534ffb14687672bc627742dd50f93894
Pabs Monestel <111648020+pmonestel@users.noreply.github.com>
2022-08-20 09:58:49 -0600

    Creación carpeta práctica 1
    Se agregan los archivos de la práctica 1

---
+++ practica1/js/index.js
@@ -0,0 +1,8 @@
+let h2Nombre = document.getElementById('nombre-personaje');
+
+h2Nombre.textContent = h2Nombre.textContent.toUpperCase();
+
+//Imprimir el texto que tiene el elemento dentro
+console.log('Contenido: ', h2Nombre.textContent);
+console.log('Nombre de la clase: ', h2Nombre.className);
+console.log('Id: ', h2Nombre.id);
